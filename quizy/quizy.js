369a0e7dc0f08b546448c2413f9fc4cb4056213b
ujs128 <ugkirin@gmail.com>
2021-07-19 00:07:14 +0900

    posse課題WEEK4　遅れてすいません

---
+++ quizy/quizy.js
@@ -0,0 +1,7 @@
+'use strict'
+
+document.getElementById('wrong').addEventListener('click,process')
+document.getElementById('wrong2').addEventListener('click,process')
+document.getElementById('bingo').addEventListener('click,process2')
+
+function process
