6f7a13b03881e9afe57f7cba5024a3ff7e88fc49
ujs128 <ugkirin@gmail.com>
2021-10-18 19:42:47 +0900

    webappモーダル途中です、

---
+++ webapp/top.js
@@ -0,0 +1,29 @@
+// 'use strict'
+// let ctx = document.getElementById('studyLog');
+
+// let myBarChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22', '24', '26', '28', '30'],
+//         datasets: [
+//             {
+//                 data: [2, 4, 5, 3, 5, 6, 2, 1, 5, 7, 4, 2, 4, 3, 2, 8, 6, 4, 5, 1, 4, 6, 8, 3, 5, 3, 5, 3, 4, 7, 2],
+//                 backgroundColor: 'blue'
+//             }
+//         ]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     suggestedMax: 8,
+//                     suggestedMin: 0,
+//                     stepSize: 2,
+//                     callback: function (value, index, values) {
+//                         return value + '人'
+//                     }
+//                 }
+//             }]
+//         },
+//     }
+// });
